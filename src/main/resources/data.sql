3db99751285c1ba44eb0915c5d882beb86324bc2
Tridev Rai <tridev.rai@renegadeinsurance.com>
2023-07-20 03:27:01 +0545

    feat: updating the schema and data sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO drones(id, serial_number, model, weight_limit, battery_capacity, state)
-VALUES (1, 'someSerialNumber', 'MIDDLEWEIGHT', 2.5, 10, 'LOADED');
+VALUES (1, 'someSerialNumber', 'MIDDLEWEIGHT', 2.50, 10, 'LOADED');
+
+-- negative data that should fail
+-- INSERT INTO drones(id, serial_number, model, weight_limit, battery_capacity, state)
+-- VALUES (2, 'someSerialNumfsfsdfsdfsdfsdfsdfsdfsdfsdf24234rwefdsfw23redwssw23refdew234rfew3234refve34gre4t54gtrber', 'MIDDLEWEIGHT', 345.0, 10, 'LOADED');
