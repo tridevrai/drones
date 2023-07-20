af9f3322244ff225dac2b572986644f4f7332980
Tridev Rai <tridev.rai@renegadeinsurance.com>
2023-07-20 23:00:13 +0545

    feat: Drone service skeleton added along with the dto

---
+++ src/main/java/com/musala/drones/dto/DroneDto.java
@@ -0,0 +1,16 @@
+package com.musala.drones.dto;
+
+import com.musala.drones.models.enums.Model;
+import com.musala.drones.models.enums.State;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+public class DroneDto implements Serializable {
+  Long id;
+  String serialNumber;
+  Model model;
+  double weightLimit;
+  int batteryCapacity;
+  State state;
+}
