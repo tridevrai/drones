aaa7647e614d5a4e1d08ac2ad8155a5d84fe94b2
Tridev Rai <tridev.rai@renegadeinsurance.com>
2023-07-22 01:53:22 +0545

    feat: Global exception handler for bad request and bean validation failure

---
+++ src/main/java/com/musala/drones/exceptions/handler/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.musala.drones.exceptions.handler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+  @Override
+  protected ResponseEntity<Object> handleHttpMessageNotReadable(
+      HttpMessageNotReadableException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    return new ResponseEntity<>(ex.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+  }
+
+  /**
+   * This handler prepares the JSR validation error message on the {@link
+   * com.musala.drones.dto.DroneDto} AgentCredentialDto
+   *
+   * @param ex the exception
+   * @param headers the headers to be written to the response
+   * @param status the selected response status
+   * @param request the current request
+   * @return {@link Object}
+   */
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+
+    List<String> errorMessage =
+        ex.getBindingResult().getAllErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.toList());
+    return new ResponseEntity<>(String.join(",\n", errorMessage), HttpStatus.BAD_REQUEST);
+  }
+}
